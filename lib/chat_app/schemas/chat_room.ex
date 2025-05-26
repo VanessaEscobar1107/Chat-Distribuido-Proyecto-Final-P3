20549585d054117934ba7b532ee2ea51ed416958
VanessaEscobar1107 <escobarvanessa210@gmail.com>
2025-05-26 10:40:49 -0500

    actualizar chat_room.ex

--- lib/chat_app/schemas/chat_room.ex
+++ lib/chat_app/schemas/chat_room.ex
@@ -1,10 +1,11 @@
+# Este modulo sirve para definir el esquema de la sala de chat.
 defmodule ChatApp.Schemas.ChatRoom do
-  use Ecto.Schema
+  use Ecto.Schema                        # use Ecto.Schema para definir el esquema
   alias ChatApp.Schemas.Message
 
   schema "chat_rooms" do
-    field :name, :string
-    has_many :messages, Message
-    timestamps()
+    field :name, :string             # field para el nombre de la sala
+    has_many :messages, Message      # has_many para las relaciones con los mensajes
+    timestamps()                     # timestamps para crear campos de fecha y hora
   end
 end
