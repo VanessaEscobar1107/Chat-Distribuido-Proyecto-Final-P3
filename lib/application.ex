3a4edaa1c590a57a4251a7f7ef5a400749b27b4b
VanessaEscobar1107 <escobarvanessa210@gmail.com>
2025-05-26 10:57:37 -0500

    actulizar application.ex

--- lib/application.ex
+++ lib/application.ex
@@ -1,20 +1,21 @@
+# Este modulo inicia la aplicación ChatApp y gestiona los procesos supervisados.
 defmodule ChatApp.Application do
   use Application
   require Logger
 
   def start(_type, _args) do
     # Cargar la configuración de Libcluster para gestionar los nodos
-    topologies = Application.get_env(:libcluster, :topologies, [])   # Topologies es para la configuración de Libcluster
+    topologies = Application.get_env(:libcluster, :topologies, [])   # Topologies sirve para la configuración de Libcluster
 
     # Children es para definir los procesos que se iniciarán al arrancar la aplicación
     children = [
       {Cluster.Supervisor, [topologies, [name: ChatApp.ClusterSupervisor]]}, # Supervisor de nodos
-      ChatApp.Repo,  # Base de datos
-      ChatApp.Server, # Servidor de chat
+      ChatApp.Repo,       # Base de datos
+      ChatApp.Server,     # Servidor de chat
       {ChatApp.Auth,[]},  # Autenticación de usuarios, pasando argumentos
     ]
 
-    # opts es para definir las opciones del supervisor
+    # opts sirve para definir las opciones del supervisor
     opts = [strategy: :one_for_one, name: ChatApp.Supervisor]
 
    case Supervisor.start_link(children, opts) do
